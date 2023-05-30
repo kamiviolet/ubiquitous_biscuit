7543eee54bc70d93ff3e0f6181798a3222dbc802
kamiviolet <kamiviolet@gmail.com>
2023-05-30 16:29:05 +0100

    add utils to get topics for Nav and get article for single Article

--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,15 @@
 import axios from 'axios'
 
-const ncNewsAPI = axios.create({ baseURL: 'https://nc-news-au6b.onrender.com' })
+const ncNewsAPI = axios.create({ baseURL: 'https://nc-news-au6b.onrender.com/api' })
 
 export const getAllArticles = () => {
-    return ncNewsAPI.get('/api/articles').then(({data}) => data);
+    return ncNewsAPI.get('/articles').then(({data}) => data);
 };
+
+export const getAllTopics = () => {
+    return ncNewsAPI.get('/topics').then(({data}) => data);
+};
+
+export const fetchArticleByArticleId = (articleId) => {
+    return ncNewsAPI.get(`/articles/${articleId}`).then(({data}) => data);
+}
