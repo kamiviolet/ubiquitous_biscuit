2c9be1554c1c541bb24d975bfe8f89af62ca2adf
kamiviolet <kamiviolet@gmail.com>
2023-05-31 14:45:05 +0100

    separate css for new-comment-form

--- src/components/NewCommentForm.jsx
+++ src/components/NewCommentForm.jsx
@@ -1,4 +1,5 @@
 import { HiUser } from "react-icons/hi2";
+import '../css/new_comment_form.css'
 
 export default function NewCommentForm() {
     return (
@@ -6,7 +7,8 @@ export default function NewCommentForm() {
             <div className="avatar" role="avatar"><HiUser /></div>
             <label for="username" className="username_label required">Username: </label>
             <input name="username" id="username" type="text" required/>
-            <label for="comment" className="comment_label" >Comment: </label><textarea />
+            <label for="comment" className="comment_label" >Comment: </label>
+            <textarea id="comment" />
             <button type="submit">Add comment</button>
         </form>
     )
