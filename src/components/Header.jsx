b55368f015207a4b4f0d3e28f770efa862b0f4fe
kamiviolet <kamiviolet@gmail.com>
2023-05-31 10:43:57 +0100

    add link to home to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 export default function Header() {
-    return (<header><h1>NC-News</h1></header>)
+    return (<header><h1><Link to="/">NC-News</Link></h1></header>)
 }
