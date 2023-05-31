83dafee2c9ac7f4db8d8e2b486410b8381b90904
kamiviolet <kamiviolet@gmail.com>
2023-05-31 14:39:17 +0100

    add form for postComment function, with basic style

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -1,8 +1,9 @@
 import '../css/list_of_comments.css'
-import {convertDate} from "../../utils/utils"
-import UpvoteBtn from "../components/UpvoteBtn"
 import { fetchCommentsByArticleId } from '../../utils/utils'
 import { useEffect, useState } from "react";
+import ListOfComments from '../components/ListOfComments'
+import NewCommentForm from '../components/NewCommentForm'
+
 
 export default function CommentSection({article_id}) {
     const [listOfComments, setListOfComments] = useState([])
@@ -15,25 +16,13 @@ export default function CommentSection({article_id}) {
     }, [])
 
     if (isLoading) {
-        return  <div id="loading_page">Loading...<br />Don't worry, it may take some time. Thank you for your patience.</div>
+        return  <div className='loading_page'>Loading...<br />Don't worry, it may take some time. Thank you for your patience.</div>
     }
 
     return (
         <section id="comments" role="comment_section">
-            <h3>Comments</h3>
-            <ul className='list_of_comments'>
-                {listOfComments.map(comment => {
-                    return (
-                        <li key={"comment_" + comment.comment_id} className='comment_card'>
-                            <p role="comment_id" aria-hidden>{comment.comment_id}</p>
-                            <p role="comment_body" aria-roledescription="comment_body">{comment.body}</p>
-                            <p role="author" aria-roledescription="author">{comment.author}</p>
-                            <p role="date" aria-roledescription="created_at">{convertDate(comment.created_at)}</p>
-                            <UpvoteBtn type="comment" id={comment.comment_id} votes={comment.votes} />
-                        </li>
-                    )
-                })}
-            </ul>
+            <NewCommentForm />
+            <ListOfComments listOfComments={listOfComments} />
         </section>
     )
 }
---
+++ src/components/NewCommentForm.jsx
@@ -0,0 +1,12 @@
+import { HiUser } from "react-icons/hi2";
+
+export default function NewCommentForm() {
+    return (
+        <form className="new_comment" role="new_comment_form">
+            <div className="avatar" role="avatar"><HiUser /></div>
+            <label for="username" className="username_label required">Username: </label><input name="username" id="username" type="text" required/>
+            <label for="comment" className="comment_label" >Comment: </label><textarea />
+            <button type="submit">Add comment</button>
+        </form>
+    )
+}
