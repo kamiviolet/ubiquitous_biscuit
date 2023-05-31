9a915d159f4ed37d3930fe4d7b53ca6fa5014548
kamiviolet <kamiviolet@gmail.com>
2023-05-31 14:40:57 +0100

    add classnames to input fields in form

--- src/components/NewCommentForm.jsx
+++ src/components/NewCommentForm.jsx
@@ -4,7 +4,8 @@ export default function NewCommentForm() {
     return (
         <form className="new_comment" role="new_comment_form">
             <div className="avatar" role="avatar"><HiUser /></div>
-            <label for="username" className="username_label required">Username: </label><input name="username" id="username" type="text" required/>
+            <label for="username" className="username_label required">Username: </label>
+            <input name="username" id="username" type="text" required/>
             <label for="comment" className="comment_label" >Comment: </label><textarea />
             <button type="submit">Add comment</button>
         </form>
