f54c8eab40a3ba020a4484b5fadaca6194f5b35f
kamiviolet <kamiviolet@gmail.com>
2023-06-01 13:48:20 +0100

    refactor to avoid infinite loop

--- src/routes/ListOfAllArticles.jsx
+++ src/routes/ListOfAllArticles.jsx
@@ -3,27 +3,35 @@ import Subheader from '../components/Subheader'
 import ArticleSummary from '../components/ArticleSummary'
 import { getAllArticles } from '../../utils/utils'
 import '../css/list_of_articles.css'
+import FilterSorter from '../components/FilterSorter'
 
-export default function ListOfAllArticles({topic_name}) {
+export default function ListOfAllArticles({topic}) {
     const [listOfArticles, setListOfArticles] = useState([]);
     const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
-        getAllArticles(topic_name).then(({articles}) => setListOfArticles(articles)).then(() => setIsLoading(false))
-    }, [listOfArticles]);
-    
+        getAllArticles(topic)
+            .then(({articles}) => setListOfArticles(articles))
+            .then(() => setIsLoading(false))
+    }, [topic]);
+
     if (isLoading) {
         return  <div className='loading_page'>Loading...<br />Thank you for your patience.</div>
     }
 
     return (
         <main>
-            <Subheader title={topic_name? topic_name : "All topics"} />
+            <Subheader title={topic? topic : "All topics"} />
+            <FilterSorter />
             <ul className='list_of_articles'>
                 {
                     (!listOfArticles) ? <></>
                     : listOfArticles.map(article => {
-                        return (<li key={"article_" + article.article_id} className='article_card'><ArticleSummary topic={topic_name} article={article}/></li>)
+                        return (
+                            <li key={"article_" + article.article_id} className='article_card'>
+                                <ArticleSummary topic={topic} article={article}/>
+                            </li>
+                        )
                     })
                 }
             </ul>
--- src/routes/ListOfArticlesByTopic.jsx
+++ src/routes/ListOfArticlesByTopic.jsx
@@ -5,6 +5,6 @@ export default function ListOfArticlesByTopic() {
     const {topic_name} = useParams();
 
     return (
-        <ListOfAllArticles topic_name={topic_name} />
+        <ListOfAllArticles topic={topic_name} />
     )
 }
