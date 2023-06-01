31ca545c4ee4e9e1c24e280fe2c7c29f79e81097
kamiviolet <kamiviolet@gmail.com>
2023-06-01 17:24:03 +0100

    minor adjustment

--- src/routes/ListOfArticlesByTopic.jsx
+++ src/routes/ListOfArticlesByTopic.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 import ListOfAllArticles from './ListOfAllArticles'
 
 export default function ListOfArticlesByTopic() {
-    const {topic_name} = useParams();
+    const { topic_name } = useParams();
 
     return (
         <ListOfAllArticles topic={topic_name} />
