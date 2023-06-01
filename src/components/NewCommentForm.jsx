e0ed390ef45644d7ab6d99db1df32692fc34009d
kamiviolet <kamiviolet@gmail.com>
2023-06-01 11:32:32 +0100

    refactor: disable btn after submitting new comment, add dropdown for existing users, make textarea required

--- src/components/NewCommentForm.jsx
+++ src/components/NewCommentForm.jsx
@@ -1,27 +1,56 @@
 import { HiUser } from "react-icons/hi2";
-import { postNewCommentByArticleId } from "../../utils/utils"
+import { postNewCommentByArticleId, fetchUsers } from "../../utils/utils"
+import { useEffect, useState } from 'react'
 import '../css/new_comment_form.css'
 
 export default function NewCommentForm({article_id, newComment, setNewComment, listOfComments, setListOfComments}) {
+    const [allUsers, setAllUsers] = useState([]);
+    const [posting, setPosting] = useState(false);
+
     const handleNewComment = (e) => {
             e.preventDefault();
+            setPosting(true)
+
             postNewCommentByArticleId(article_id, newComment)
                 .then(({comment}) => {
                     setNewComment({username: "", body: ""});
-                    setListOfComments((listOfComments) => {
-                        return [comment, ...listOfComments]
-                    })
+                    setListOfComments((listOfComments) => [comment, ...listOfComments])
+                })
+                .then(() => {
+                    setPosting(false)
                 })
     }
+
+    useEffect(() => {
+        fetchUsers().then(({users}) => setAllUsers(users))
+    }, [])
 
     return (
         <form className="new_comment" role="new_comment_form" onSubmit={handleNewComment}>
             <div className="avatar" role="avatar"><HiUser /></div>
             <label htmlFor="username" className="username_label required">Username: </label>
-            <input value={newComment.username} onChange={(e)=> setNewComment({...newComment, username: e.target.value})} name="username" id="username" type="text" required/>
-            <label htmlFor="comment" className="comment_label" >Comment: </label>
-            <textarea value={newComment.body} onChange={(e)=> setNewComment({...newComment, body: e.target.value})} id="comment" />
-            <button type="submit">Add comment</button>
+            <select
+                className="input_field"
+                onChange={(e)=> setNewComment({...newComment, username: e.target.value})}
+                name="username"
+                id="username"
+                type="text"
+                value={newComment.username}
+                required
+            >
+                <option value=""></option>
+                {allUsers.map(user => <option value={user.username} key={user.username}>{user.username}</option>)}
+            </select>
+            <label htmlFor="comment" className="comment_label required" >Comment: </label>
+            <textarea
+                className="input_field"
+                value={newComment.body}
+                onChange={(e)=> setNewComment({...newComment, body: e.target.value})}
+                id="comment"
+                disabled={posting? true: false} 
+                required
+            />
+            <button disabled={posting? true: false} className="submit_btn" type="submit">Add comment</button>
         </form>
     )
 }
