ccef69251e6c6a9b466eaa955d31cc1e60c66cb5
kamiviolet <kamiviolet@gmail.com>
2023-06-02 16:43:00 +0100

    minor adjust to style and functionality of sorting, disable submit button when not changing params

--- src/components/FilterSorter.jsx
+++ src/components/FilterSorter.jsx
@@ -13,24 +13,28 @@ export default function FilterSorter({params, setParams}) {
             sort_by: sort, order: order
         })
         setSearchParams(params);
+        setOrder('')
+        setSort('')
     }
 
     return (
         <form id="filter_sorter" action="" onSubmit={handleSorting}>
-            <label htmlFor="sort">Sort By
-                <select name="sort_by" onChange={(e)=>setSort(e.target.value)}>
+            <label htmlFor="sort">Sort By (date created by default)
+                <select value={sort} name="sort_by" onChange={(e)=>setSort(e.target.value)}>
+                    <option value=""></option>
                     <option value="created_at">Date created</option>
                     <option value="comment_count">comment count</option>
                     <option value="votes">votes</option>
                 </select>
             </label>
-            <label htmlFor="order">Order By
-            <select name="order" onChange={(e)=>setOrder(e.target.value)}>
+            <label htmlFor="order">Order By (descending by default)
+            <select value={order} name="order" onChange={(e)=>setOrder(e.target.value)}>
+                <option value=""></option>
                 <option value="asc">Ascending</option>
                 <option value="desc">Descending</option>
             </select>
             </label>
-            <button type="submit">Submit</button>
+            <button type="submit" disabled={!sort && !order ? true : false}>Submit</button>
         </form>
     )
 }
